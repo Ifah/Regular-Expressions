a3333a7a0aab730655061717634e1a33db441d4b
Mohamed Ifah <ifaa.x.h@gmail.com>
2015-02-05 15:33:02 +0500

    Regular Expressions
    The basics of regular expressions.

---
+++ 7 JavaScript _ PHP Examples/index.php
@@ -0,0 +1,29 @@
+<!-- 
+php example
+<?php
+
+$regex = '/test(ing|ed|er)?/';
+
+$string = 'testing';
+
+$match = preg_match($regex, $string);
+
+if($match){
+	echo 'Matched!';
+}else{
+	echo 'No Match!';
+}
+
+?> -->
+
+<!-- javascript example -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<script src="main.js"></script>
+</body>
+</html>
---
+++ 7 JavaScript _ PHP Examples/main.js
@@ -0,0 +1,16 @@
+(function(){
+	/*
+	var string = 'hello world';
+
+	if(string.match(/hello/)){
+		console.log('String matches!');
+	}
+*/
+	var string = '123456';
+
+	if(string.match(/[0-5]/)){
+		console.log('String matches!');
+	}
+
+
+})();
